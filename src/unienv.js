72303dc69c618deb173500984fbea428ffaa72ba
Sebastian Fastner <s.fastner@sebastian-software.de>
2019-07-26 21:18:31 +0200

    feat(cli): Add context parameter

--- src/unienv.js
+++ src/unienv.js
@@ -1,9 +1,39 @@
+/* eslint-disable global-require */
 import crossEnv from "cross-env"
+import yargs from "yargs"
 
-import "."
+function getParameter() {
+  const args = yargs
+    .option('context', {
+      alias: 'c',
+      type: "string"
+    })
+    .option('verbose', {
+      alias: 'v',
+      default: false,
+      type: "boolean"
+    })
+    .argv
 
-function executeCommand(argv) {
-  crossEnv(argv.slice(2))
+  return args
 }
 
-executeCommand(process.argv)
+function executeCommand() {
+  const args = getParameter()
+  if (args.context) {
+    process.env.ENV_CONTEXT = args.context
+  }
+
+  if (args.verbose) {
+    process.stderr.write("Load .env files...\n")
+  }
+  // Executed in bin folder, so require top level
+  require("..")
+
+  if (args.verbose) {
+    process.stderr.write(`Execute "${args._.join(" ")}"...\n`)
+  }
+  crossEnv(args._)
+}
+
+executeCommand()
